46d054579c67e3a861a869552bb5ab877aa631b7
tsyganovskiy <tsyganovskiySS@gmail.com>
2017-08-10 10:19:04 +0300

    Рефакторинг теста по проверке перехода на внешнюю страницу

--- src/test/java/ru/stqa/NewPageTest.java
+++ src/test/java/ru/stqa/NewPageTest.java
@@ -3,9 +3,11 @@ package ru.stqa;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.Set;
 
 public class NewPageTest extends TestBase{
@@ -16,19 +18,17 @@ public class NewPageTest extends TestBase{
         wd.findElement(By.linkText("Countries")).click();
         wd.findElement(By.linkText("Add New Country")).click();
 
-        goToOuterPage(By.cssSelector("[href $=\"alpha-2\"]"));
-        goToOuterPage(By.cssSelector("[href $=\"alpha-3\"]"));
-        goToOuterPage(By.cssSelector("[href $=\"Regular_expression\"]"));
-        goToOuterPage(By.cssSelector("[href $=\"address-formats.html\"]"));
-        goToOuterPage(By.cssSelector("[href $=\"Regular_expression\"]"));
-        goToOuterPage(By.cssSelector("[href $=\"currency_and_language\"]"));
-        goToOuterPage(By.cssSelector("[href $=\"List_of_country_calling_codes\"]"));
+        List<WebElement> links = wd.findElements(By.cssSelector(".fa.fa-external-link"));
+
+        for (WebElement link: links){
+            goToOuterPage(link);
+        }
     }
 
-    private void goToOuterPage(By locator) {
+    private void goToOuterPage(WebElement link) {
         String originalWindow = wd.getWindowHandle();
         Set<String> existingWindows = wd.getWindowHandles();
-        wd.findElement(locator).click();
+        link.click();
         new WebDriverWait(wd, 5)
                 .until(ExpectedConditions.numberOfWindowsToBe(existingWindows.size() + 1));
         Set<String> newExistingWindows = wd.getWindowHandles();
